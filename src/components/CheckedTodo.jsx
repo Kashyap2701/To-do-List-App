ace5c32a7424fb1930afaa0dcc82e0d31f4f9bbd
Kashyap Patel <kashyap.p@simformsolutions.com>
2023-04-27 14:17:07 +0530

    add opacity to completed todo

--- src/components/CheckedTodo.jsx
+++ src/components/CheckedTodo.jsx
@@ -1,10 +1,10 @@
 import style from './TodoList.module.css';
-import checked from '../assets/checked.png'
+import checked from '../assets/checked.png';
 
 function CheckedTodo() {
   return (
     <div className={style.todo}>
-        <p>Read an Artical</p>
+        <p className={style.completed}>Read an Artical</p>
         <div>
             <img width={25} src={checked} alt="" />
         </div>
